8413c70618d92436f8156c38c0ca0d0ccef0e2b4
Nico <deminas.nicolas@gmail.com>
2022-02-03 10:45:55 -0300

    Agregado modo cluster y nginx

--- index.js
+++ index.js
@@ -1,253 +1,267 @@
 const express = require("express");
 const app = express();
-
 const parseArg = require("minimist");
-const http = require("http");
-const server = http.createServer(app);
-const knex = require("./db");
-const Contenedor = require("./contenedor");
-const Mensajes = require("./mensajes");
-const random = require("./routes/randoms");
-const { normalize, denormalize, schema } = require("normalizr");
-const util = require("util");
-const session = require("express-session");
-const mongoStore = require("connect-mongo");
-const passport = require("passport");
-const bcrypt = require("bcrypt");
-const LocalStrategy = require("passport-local").Strategy;
-const { userModel } = require("./db");
-const dotenv = require("dotenv");
-const { createHash } = require("crypto");
-
-const io = require("socket.io")(server);
-
-const c = new Contenedor();
-const msn = new Mensajes();
-
-function print(objeto) {
-  console.log(util.inspect(objeto, false, 12, true));
-}
-
-dotenv.config();
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 const options = {
-  default: { port: 8080 },
-  alias: { p: "port" },
+  default: { port: 8080, mode: "fork" },
+  alias: { p: "port", m: "mode" },
 };
 
 let arguments = parseArg(process.argv.slice(2), options);
 
-//Normalizr
+const clusterMode = arguments.mode == "CLUSTER";
 
-const authorSchema = new schema.Entity("author", {}, { idAttribute: "id" });
-
-const messageSchema = new schema.Entity(
-  "text",
-  { author: authorSchema },
-  { idAttribute: "id" }
-);
-
-const messagesSchema = new schema.Entity(
-  "posts",
-  { text: [messageSchema] },
-  { idAttribute: "id" }
-);
-
-function modifyData(data) {
-  const newData = { id: "message", posts: data };
-  return newData;
-}
-
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.use(
-  session({
-    store: mongoStore.create({
-      mongoUrl: process.env.MONGO_CONNECT,
-    }),
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: true,
-    resave: true,
-  })
-);
-
-app.use("/api/randoms", random);
-
-const authorize = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-    return;
+if (clusterMode && cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-  res.redirect("/login");
-};
+  cluster.on("exit", () => {
+    console.log(`Process ${process.pid} died`);
+  });
+} else {
+  const http = require("http");
+  const server = http.createServer(app);
+  const knex = require("./db");
+  const Contenedor = require("./contenedor");
+  const Mensajes = require("./mensajes");
+  const random = require("./routes/randoms");
+  const { normalize, denormalize, schema } = require("normalizr");
+  const util = require("util");
+  const session = require("express-session");
+  const mongoStore = require("connect-mongo");
+  const passport = require("passport");
+  const bcrypt = require("bcrypt");
+  const LocalStrategy = require("passport-local").Strategy;
+  const { userModel } = require("./db");
+  const dotenv = require("dotenv");
+  const { createHash } = require("crypto");
 
-//PASSPORT
-app.use(passport.initialize());
-app.use(passport.session());
+  const io = require("socket.io")(server);
 
-passport.use(
-  "local-login",
-  new LocalStrategy((username, password, done) => {
-    userModel.findOne({ username: username }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        console.log(`User not found`);
-        return done(null, false);
-      }
-      if (!isValidPassword(user, password)) {
-        console.log(`Invalid password`);
-        return done(null, false);
-      }
-      return done(null, user);
-    });
-  })
-);
+  const c = new Contenedor();
+  const msn = new Mensajes();
 
-passport.use(
-  "local-signup",
-  new LocalStrategy(
-    {
-      passReqToCallback: true,
-    },
-    (req, username, password, done) => {
-      userModel.findOne({ username: username }, (err, user) => {
-        if (err) {
-          console.log(`Error in signup ${err}`);
-        }
-        if (user) {
-          console.log(`User already exists`);
-          return done(null, false);
-        }
-        const newUser = {
-          username: username,
-          password: bcrypt.hashSync(password, 10),
-        };
+  function print(objeto) {
+    console.log(util.inspect(objeto, false, 12, true));
+  }
 
-        userModel.create(newUser, (err, user) => {
-          if (err) {
-            console.log(`Error in saving user: ${err}`);
-            return done(err);
-          }
-          return done(null, user);
-        });
-      });
-    }
-  )
-);
+  dotenv.config();
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+  //Normalizr
 
-passport.deserializeUser((id, done) => {
-  userModel.findById(id, done);
-});
+  const authorSchema = new schema.Entity("author", {}, { idAttribute: "id" });
 
-function isValidPassword(user, password) {
-  return bcrypt.compareSync(password, user.password);
-}
-
-app.get("/", (req, res) => {
-  res.redirect("/home");
-});
-
-io.on("connection", async (socket) => {
-  const data = await msn.getAll();
-
-  const normalizeMessage = normalize(modifyData(data), messagesSchema);
-  const deNormalizeData = denormalize(
-    normalizeMessage.result,
-    messagesSchema,
-    normalizeMessage.entities
+  const messageSchema = new schema.Entity(
+    "text",
+    { author: authorSchema },
+    { idAttribute: "id" }
   );
 
-  socket.emit("message_back", normalizeMessage);
+  const messagesSchema = new schema.Entity(
+    "posts",
+    { text: [messageSchema] },
+    { idAttribute: "id" }
+  );
 
-  socket.on("data_msn", async (data) => {
-    let newData = denormalize(data.result, messagesSchema, data.entities);
-    await msn.save(newData.posts);
+  function modifyData(data) {
+    const newData = { id: "message", posts: data };
+    return newData;
+  }
 
-    io.sockets.emit("message_back", normalizeMessage);
-  });
+  app.use(express.json());
+  app.use(express.urlencoded());
 
-  socket.emit("infoProductos", await c.getAll());
+  app.use(
+    session({
+      store: mongoStore.create({
+        mongoUrl: process.env.MONGO_CONNECT,
+      }),
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: true,
+      resave: true,
+    })
+  );
 
-  socket.on("newProduct", async (data) => {
-    await c.save(data);
-    console.log(`data guardada`);
-    io.sockets.emit("infoProductos", await c.getAll());
-  });
-});
+  app.use("/api/randoms", random);
 
-app.get("/home", authorize, (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(`${__dirname}/public/login.html`);
-});
-
-app.get("/signup", (req, res) => {
-  res.sendFile(`${__dirname}/public/signup.html`);
-});
-
-app.get("/getUsers", (req, res) => {
-  res.send(user);
-});
-
-app.post(
-  "/signup",
-  passport.authenticate("local-signup", {
-    successRedirect: "/login",
-    failureRedirect: "/signup",
-  })
-);
-
-app.post(
-  "/login",
-  passport.authenticate("local-login", {
-    successRedirect: "/home",
-    failureRedirect: "/login",
-  })
-);
-
-app.get("/logout", (req, res) => {
-  req.logOut();
-  res.redirect("/login");
-});
-
-app.put("/updateProduct/:id", async (req, res) => {
-  await c.update(req.params.id, req.body);
-  res.send(`Producto id=${req.params.id} actualizado con exito`);
-});
-
-app.delete("/deleteProduct/:id", async (req, res) => {
-  await c.delete(req.params.id);
-  res.send(`Producto id=${req.params.id} eliminado con exito`);
-});
-
-app.get("/api/productos-test", async (req, res) => {
-  res.send(await c.getFakerProducts());
-});
-
-app.get("/info", (req, res) => {
-  let data = {
-    "argumentos de entrada": process.argv.slice(2),
-    "sistema opertativo": process.platform,
-    "version de node": process.version,
-    rss: process.memoryUsage().rss,
-    path: process.execPath,
-    processId: process.pid,
-    "carpeta proyecto": process.cwd(),
+  const authorize = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      next();
+      return;
+    }
+    res.redirect("/login");
   };
-  res.json({ data });
-});
 
-app.use(express.static(__dirname + "/public/"));
+  //PASSPORT
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-server.listen(arguments.port, () => {
-  console.log(`Server run on port ${arguments.port}`);
-});
+  passport.use(
+    "local-login",
+    new LocalStrategy((username, password, done) => {
+      userModel.findOne({ username: username }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          console.log(`User not found`);
+          return done(null, false);
+        }
+        if (!isValidPassword(user, password)) {
+          console.log(`Invalid password`);
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        passReqToCallback: true,
+      },
+      (req, username, password, done) => {
+        userModel.findOne({ username: username }, (err, user) => {
+          if (err) {
+            console.log(`Error in signup ${err}`);
+          }
+          if (user) {
+            console.log(`User already exists`);
+            return done(null, false);
+          }
+          const newUser = {
+            username: username,
+            password: bcrypt.hashSync(password, 10),
+          };
+
+          userModel.create(newUser, (err, user) => {
+            if (err) {
+              console.log(`Error in saving user: ${err}`);
+              return done(err);
+            }
+            return done(null, user);
+          });
+        });
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    userModel.findById(id, done);
+  });
+
+  function isValidPassword(user, password) {
+    return bcrypt.compareSync(password, user.password);
+  }
+
+  app.get("/", (req, res) => {
+    res.redirect("/home");
+  });
+
+  io.on("connection", async (socket) => {
+    const data = await msn.getAll();
+
+    const normalizeMessage = normalize(modifyData(data), messagesSchema);
+    const deNormalizeData = denormalize(
+      normalizeMessage.result,
+      messagesSchema,
+      normalizeMessage.entities
+    );
+
+    socket.emit("message_back", normalizeMessage);
+
+    socket.on("data_msn", async (data) => {
+      let newData = denormalize(data.result, messagesSchema, data.entities);
+      await msn.save(newData.posts);
+
+      io.sockets.emit("message_back", normalizeMessage);
+    });
+
+    socket.emit("infoProductos", await c.getAll());
+
+    socket.on("newProduct", async (data) => {
+      await c.save(data);
+      console.log(`data guardada`);
+      io.sockets.emit("infoProductos", await c.getAll());
+    });
+  });
+
+  app.get("/home", authorize, (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+  });
+
+  app.get("/login", (req, res) => {
+    res.sendFile(`${__dirname}/public/login.html`);
+  });
+
+  app.get("/signup", (req, res) => {
+    res.sendFile(`${__dirname}/public/signup.html`);
+  });
+
+  app.get("/getUsers", (req, res) => {
+    res.send(user);
+  });
+
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/login",
+      failureRedirect: "/signup",
+    })
+  );
+
+  app.post(
+    "/login",
+    passport.authenticate("local-login", {
+      successRedirect: "/home",
+      failureRedirect: "/login",
+    })
+  );
+
+  app.get("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/login");
+  });
+
+  app.put("/updateProduct/:id", async (req, res) => {
+    await c.update(req.params.id, req.body);
+    res.send(`Producto id=${req.params.id} actualizado con exito`);
+  });
+
+  app.delete("/deleteProduct/:id", async (req, res) => {
+    await c.delete(req.params.id);
+    res.send(`Producto id=${req.params.id} eliminado con exito`);
+  });
+
+  app.get("/api/productos-test", async (req, res) => {
+    res.send(await c.getFakerProducts());
+  });
+
+  app.get("/info", (req, res) => {
+    let data = {
+      "argumentos de entrada": process.argv.slice(2),
+      "sistema opertativo": process.platform,
+      "version de node": process.version,
+      rss: process.memoryUsage().rss,
+      path: process.execPath,
+      processId: process.pid,
+      "carpeta proyecto": process.cwd(),
+      procesadores: numCPUs,
+    };
+    res.json({ data });
+  });
+
+  app.use(express.static(__dirname + "/public/"));
+
+  server.listen(arguments.port, () => {
+    console.log(`Server run on port ${arguments.port}`);
+  });
+}

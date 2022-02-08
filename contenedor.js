26d2dea4804104395ba8ec4fbcdc79354283f0dd
Nico <deminas.nicolas@gmail.com>
2022-02-08 10:30:42 -0300

    sacada config de MySql

--- contenedor.js
+++ contenedor.js
@@ -8,58 +8,58 @@ class Contenedor {
     this.product = [];
   }
 
-  async save(product) {
-    await knex("products").insert(product);
-  }
+  // async save(product) {
+  //   await knex("products").insert(product);
+  // }
 
-  async getById(id) {
-    let data = [];
-    await knex
-      .from("products")
-      .where({ id: id })
-      .then((res) => {
-        data = res;
-      });
-    return data;
-  }
+  // async getById(id) {
+  //   let data = [];
+  //   await knex
+  //     .from("products")
+  //     .where({ id: id })
+  //     .then((res) => {
+  //       data = res;
+  //     });
+  //   return data;
+  // }
 
-  async getAll() {
-    let data = [];
-    await knex
-      .select("nombre", "descripcion", "codigo", "foto", "precio", "stock")
-      .from("products")
-      .then((res) => {
-        data = res;
-      });
-    return data;
-  }
+  // async getAll() {
+  //   let data = [];
+  //   await knex
+  //     .select("nombre", "descripcion", "codigo", "foto", "precio", "stock")
+  //     .from("products")
+  //     .then((res) => {
+  //       data = res;
+  //     });
+  //   return data;
+  // }
 
-  async update(id, obj) {
-    console.log(obj);
-    knex
-      .from("products")
-      .where({ id: id })
-      .update({
-        nombre: obj.nombre,
-        descripcion: obj.descripcion,
-        codigo: obj.codigo,
-        foto: obj.foto,
-        precio: obj.precio,
-        stock: obj.stock,
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  }
+  // async update(id, obj) {
+  //   console.log(obj);
+  //   knex
+  //     .from("products")
+  //     .where({ id: id })
+  //     .update({
+  //       nombre: obj.nombre,
+  //       descripcion: obj.descripcion,
+  //       codigo: obj.codigo,
+  //       foto: obj.foto,
+  //       precio: obj.precio,
+  //       stock: obj.stock,
+  //     })
+  //     .then((res) => {
+  //       console.log(res);
+  //     });
+  // }
 
-  async delete(id) {
-    knex("products")
-      .where({ id: id })
-      .del()
-      .then((res) => {
-        console.log(res);
-      });
-  }
+  // async delete(id) {
+  //   knex("products")
+  //     .where({ id: id })
+  //     .del()
+  //     .then((res) => {
+  //       console.log(res);
+  //     });
+  // }
 
   async getFakerProducts() {
     for (let i = 0; i < 5; i++) {
